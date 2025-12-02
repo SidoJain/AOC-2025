a925abbf8e319f428e80f637834d3decf1c740e8
Siddharth Jain <sidojain30705@gmail.com>
2025-12-02 10:41:46 +0530

    complete day 2

---
+++ d2p1.py
@@ -0,0 +1,24 @@
+def is_invalid_id(id_num: int) -> bool:
+    s = str(id_num)
+    if len(s) % 2 == 1:
+        return False
+    half = len(s) // 2
+    return s[:half] == s[half:]
+
+def parse_ranges(ranges_str: str) -> list[tuple[int, int]]:
+    ranges = []
+    for range in ranges_str.split(','):
+        start, end = range.split('-')
+        ranges.append((int(start), int(end)))
+    return ranges
+
+def sum_invalid_ids(ranges: list[tuple[int, int]]) -> int:
+    invalid_ids = [id_num for start, end in ranges for id_num in range(start, end + 1) if is_invalid_id(id_num)]
+    return sum(invalid_ids)
+
+with open("../inputs/input2.txt", "r") as file:
+    input_ranges = file.read()
+
+ranges = parse_ranges(input_ranges)
+result = sum_invalid_ids(ranges)
+print(result)
